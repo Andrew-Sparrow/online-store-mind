1627319db7c20ff86d3717756a105b250fa521f7
Andrew-Sparrow <8911102@gmail.com>
2023-05-09 21:56:54 +0500

    start function was added for DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 require('dotenv').config();
+const sequelize = require('./db');
 
 const PORT = process.env.PORT;
 const app = express();
 
-app.listen(process.env.PORT, () => console.log(`Server started ${ PORT }!`));
+
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(process.env.PORT, () => console.log(`Server started ${ PORT }!`));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+start();
