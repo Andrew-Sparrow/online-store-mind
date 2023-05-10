5a41be575ae5a086725fbb7590317f985f07bc1e
Andrew-Sparrow <8911102@gmail.com>
2023-05-10 13:20:08 +0500

    delets unused block of code

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -12,17 +12,6 @@ class productController {
       img.mv(path.resolve(__dirname, '..', 'static', fileName));
       const device = await Product.create({ title, price, img: fileName });
 
-      if (info) {
-        info = JSON.parse(info);
-        info.forEach(i =>
-          DeviceInfo.create({
-            title: i.title,
-            description: i.description,
-            deviceId: device.id
-          })
-        );
-      }
-
       return res.json(device);
     } catch (e) {
       next(ApiError.badRequest(e.message));
@@ -32,30 +21,19 @@ class productController {
   async getAll(req, res) {
     let { brandId, typeId, limit, page } = req.query;
     page = page || 1;
-    limit = limit || 9;
+    limit = limit || 10;
     let offset = page * limit - limit;
-    let devices;
-    if (!brandId && !typeId) {
-      devices = await Product.findAndCountAll({ limit, offset });
-    }
-    if (brandId && !typeId) {
-      devices = await Product.findAndCountAll({ where: { brandId }, limit, offset });
-    }
-    if (!brandId && typeId) {
-      devices = await Product.findAndCountAll({ where: { typeId }, limit, offset });
-    }
-    if (brandId && typeId) {
-      devices = await Product.findAndCountAll({ where: { typeId, brandId }, limit, offset });
-    }
-    return res.json(devices);
+
+    let products = await Product.findAndCountAll({ where: { typeId, brandId }, limit, offset });
+
+    return res.json(products);
   };
 
   async getOne(req, res) {
     const { id } = req.params;
     const device = await Product.findOne(
       {
-        where: { id },
-        include: [{ model: DeviceInfo, as: 'info' }]
+        where: { id }
       },
     )
     return res.json(device);
