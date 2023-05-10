7d25845c228b7a83ef77c41f0ac49b29e1a7aebe
Andrew-Sparrow <8911102@gmail.com>
2023-05-10 12:28:21 +0500

    adds model for DB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 const sequelize = require('./db');
+const models = require('./models/models');
+
 
 const PORT = process.env.PORT;
 const app = express();
---
+++ server/models/models.js
@@ -0,0 +1,53 @@
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
+
+const Basket = sequelize.define(
+  'basket', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  }
+})
+
+const BasketProduct = sequelize.define(
+  'basket_product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  }
+});
+
+const Product = sequelize.define(
+  'product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+},
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false
+  }
+});
+
+Basket.hasMany(BasketProduct);
+BasketProduct.belongsTo(Basket);
+
+Product.hasMany(BasketProduct)
+BasketProduct.belongsTo(Product)
+
+module.exports = {
+  Basket,
+  BasketProduct,
+  Product
+};
