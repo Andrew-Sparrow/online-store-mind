5c19685fb9eb2b43a02193c05525c5855b0a5baf
Andrew-Sparrow <8911102@gmail.com>
2023-05-12 12:38:02 +0500

    adds width to to ProductItem

--- Readme.md
+++ Readme.md
@@ -3,8 +3,6 @@
 
 ## Training project
 
-
-
 ---
 
 ### Description:
--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Col } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
 import Image from "react-bootstrap/Image";
 import { useNavigate } from "react-router-dom";
 
@@ -10,11 +11,13 @@ const ProductItem = ({ product }) => {
   const navigate = useNavigate();
 
   return (
-    <Col className={"mt-3"} onClick={() => navigate(PRODUCT_ROUTE + '/' + product.id)}>
+    <Col className={"mt-3 col-xs-4"} onClick={() => navigate(PRODUCT_ROUTE + '/' + product.id)}>
         <Card style={{ width: 150, cursor: 'pointer' }} border={"light"} className='product'>
-          <Image width={150} height={150} src={process.env.REACT_APP_API_URL + product.img} />
+        <Image width={150} height={150} src={process.env.REACT_APP_API_URL + product.img} />
+        <Container>
           <b>{product.title}</b>
           <p>{product.price} руб.</p>
+        </Container>
         </Card>
     </Col>
   );
