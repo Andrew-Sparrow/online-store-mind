95bdd79ba06e79237c789a55bf7ac692cd5a81ca
Andrew-Sparrow <8911102@gmail.com>
2023-05-13 20:53:01 +0500

    adds basket create on instance logic

--- server/controllers/basketController.js
+++ server/controllers/basketController.js
@@ -1,16 +1,29 @@
 const { Basket } = require('../models/models');
 const ApiError = require('../error/ApiError');
 
+let basket; // создаем корзину, если она еще не существует
+
 
 class basketController {
-  async create(req, res) {
-    const basket = await Basket.create();
+  async create(req, res, next) {
+    try {
+      if (!basket) {
+        basket = await Basket.create();
+      }
 
-    return res.json(basket);
+      return res.json(basket);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
   }
 
-  async getAll(req, res) {
+  async getAll(req, res, next) {
     const basket = await Basket.findAll();
+
+    if (basket.length === 0) {
+      await this.create();
+    }
+
     return res.json(basket);
   }
 };
--- server/index.js
+++ server/index.js
@@ -6,17 +6,18 @@ const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const path = require('path');
-const { Basket } = require('./models/models');
 
+const BasketController = require('./controllers/basketController');
 
 const PORT = process.env.PORT || 5030;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpload({}))
-app.use('/api', router)
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use('/api', router);
+
 
 app.use(errorHandler); // Error handler, last Middleware
 
@@ -25,13 +26,11 @@ const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
+
     app.listen(process.env.PORT, () => console.log(`Server started ${ PORT }!`));
 
-    let basket; // создаем корзину, если еще не существует
-
-    if (!basket) {
-      basket = await Basket.create();
-    }
+    await BasketController.getAll();
+    
   } catch (error) {
     console.log(error.message);
   }
